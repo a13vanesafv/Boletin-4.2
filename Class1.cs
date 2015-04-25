87e75835795286b592c3338b58b2d991b5907881
vane <a13vanesafv@iessanclemente.net>
2015-04-26 01:07:44 +0200

    Código métodos 11”.

--- Class1.cs
+++ Class1.cs
@@ -24,17 +24,46 @@ namespace Ejercicio_4_4_11
        
         public void PonerLentillas11(string colorLentillas)
         {
-  
+            if (ComprobarColor11(colorLentillas))
+                this.colorOjos11 = colorLentillas;
         }
-    
+
+        public bool ComprobarColor11(string colorLentillas)
+        {
+            return colorLentillas.Equals("azul") || colorLentillas.Equals("marron") || colorLentillas.Equals("verde");
+        }
+
         public void Engordar11(int cantidad)
         {
+            int nuevo_peso = this.peso11 + cantidad;
 
+            if (ComprobarPeso(nuevo_peso))
+            {
+                Console.WriteLine("No puede incrementar más peso!!");
+            }
+            else
+            {
+                this.peso11 = nuevo_peso;
+            }
         }
-
-        public void Adelgazar11(double cantidad)
+        public bool ComprobarPeso(int nuevo_peso)
         {
-     
+            return nuevo_peso > 180 || nuevo_peso < 40; ;
         }
+
+        public void Adelgazar11(int cantidad)
+        {
+            int nuevo_peso = this.peso11 - cantidad;
+
+            if (ComprobarPeso(nuevo_peso))
+            {
+                Console.WriteLine("No puede perder más peso!!");
+            }
+            else
+            {
+                this.peso11 = nuevo_peso;
+            }
+        }
+
     }
 }
