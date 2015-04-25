07f095f26b0ef47d368c61d65b0b248b23955f42
vane <a13vanesafv@iessanclemente.net>
2015-04-26 01:01:29 +0200

    Inicializacion11

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4_4_11
+{
+      class Persona11
+    {
+        public Persona11(int altura, int peso, string colorOjos, string colorPelo)
+        {
+            this.altura11 = altura;
+            this.colorOjos11 = colorOjos;
+            this.colorPelo11 = colorPelo;
+            this.peso11 = peso;
+        }
+
+        public int peso11;
+        public int altura11;
+        public string colorOjos11;
+        public string colorPelo11;
+
+       
+        public void PonerLentillas11(string colorLentillas)
+        {
+  
+        }
+    
+        public void Engordar11(int cantidad)
+        {
+
+        }
+
+        public void Adelgazar11(double cantidad)
+        {
+     
+        }
+    }
+}
